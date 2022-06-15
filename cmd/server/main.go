7c27c1e2c40b109a823d0dae1ce9b41fb6406a51
Abd. Majid Hamid <majid@uiconnect.id>
2022-06-15 08:15:10 +0800

    Set structure application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("App is running")
+	return nil
+}
+
 func main() {
 	fmt.Println("Test api")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("App not runing")
+		fmt.Println(err)
+	}
 }
