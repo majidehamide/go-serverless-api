11a0c4801ea180b2a770ac008c93104442852b1b
Abd. Majid Hamid <majid@uiconnect.id>
2022-09-12 09:55:01 +0800

    Structure app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Start App")
+	return nil
+}
+
 func main() {
 	fmt.Println("Belajar Go Lagi")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
