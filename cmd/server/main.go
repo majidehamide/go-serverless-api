7e9dd0c692cf446613e58199873fb8086e5770ce
Abd. Majid Hamid <majid@uiconnect.id>
2022-09-15 08:58:47 +0800

    Implementing the Transport Package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
+	transportHTTP "github.com/majidehamide/go-serverless-api/internal/transport/http"
+)
+
+// App - the struct which contains things like pointers
+// to database connections
 type App struct{}
 
+// Run - sets up our application
 func (app *App) Run() error {
-	fmt.Println("Start App")
+	fmt.Println("Setting Up Our APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 
 func main() {
-	fmt.Println("Belajar Go Lagi")
+	fmt.Println("Go REST API Course")
 	app := App{}
 	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
 		fmt.Println(err)
 	}
 }
